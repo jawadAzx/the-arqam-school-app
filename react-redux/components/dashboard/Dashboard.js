431dff76241ac66a7de36db43880648b4e8ced71
jawadAzx <23100321@lums.edu.pk>
2022-06-10 20:48:08 +0500

    dashboard done

--- react-redux/components/dashboard/Dashboard.js
+++ react-redux/components/dashboard/Dashboard.js
@@ -3,7 +3,6 @@ import {
     View,
     Text,
     Image,
-    Button,
     ScrollView,
     TextInput,
     StyleSheet,
@@ -20,10 +19,17 @@ import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { useFonts } from "expo-font";
 const { width, height } = Dimensions.get("screen");
-
 import { SafeAreaView, StatusBar, Platform } from 'react-native';
-import { Icon } from "react-native-elements";
+import { Icon, Overlay } from "react-native-elements";
+import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
+
 const Dashboard = () => {
+    const [visible, setVisible] = useState(false);
+
+    const toggleOverlay = () => {
+        Vibration.vibrate(60)
+        setVisible(!visible);
+    };
 
     return (
         <SafeAreaView style={styles.container}>
@@ -57,6 +63,21 @@ const Dashboard = () => {
             </View>
             <View style={styles.bottomContainer}>
                 <Text style={styles.bottomContainerHeading} > Notice Board </Text>
+                <View style={styles.cardContainer}>
+                    <Card style={styles.card} onPress={toggleOverlay}>
+                        <Card.Content style={styles.cardContent}>
+                            <Title style={styles.cardTitle}>Card title</Title>
+                            <Paragraph style={styles.cardParagraph}>Card content</Paragraph>
+                        </Card.Content>
+
+                        <Overlay isVisible={visible} onBackdropPress={toggleOverlay} animationType="fade"
+                            transparent>
+                            <Text>Hello from Overlay!</Text>
+                        </Overlay>
+                    </Card>
+                    
+
+                </View>
             </View>
         </SafeAreaView >
     )
@@ -107,7 +128,7 @@ const styles = StyleSheet.create({
     bottomContainer: {
         flex: 1,
         backgroundColor: '#FFF',
-        flexDirection: 'row',
+        flexDirection: 'column',
         justifyContent: 'flex-start',
         alignItems: 'flex-start',
         width: width,
@@ -121,7 +142,51 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         marginTop: height / 40,
         marginLeft: width / 25,
+    },
+    cardContainer: {
+        flex: 1,
+        backgroundColor: '#fff',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        width: width,
+        height: height / 2,
+    },
+    card: {
+        backgroundColor: '#6E5DCF',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        marginRight: width / 16,
+        marginLeft: width / 16,
+        borderTopLeftRadius: 20,
+        borderBottomRightRadius: 20,
+        marginTop: height / 30,
+        height: height / 7,
 
+
+    },
+    cardContent: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        width: width,
+        height: height / 10,
+        borderTopLeftRadius: 20,
+        borderBottomRightRadius: 20,
+    },
+    cardTitle: {
+        color: '#1560bd',
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginTop: height / 3000,
+
+    },
+    cardParagraph: {
+        color: '#D3D3D3',
+        fontSize: 15,
+        marginTop: height / 3000,
     }
 
 
